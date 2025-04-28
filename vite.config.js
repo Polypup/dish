75a03108af2e2cd3f367a83bc59d73ae48504305
PolyPup <polypup.farm@gmail.com>
2025-04-27 20:06:23 -0400

    update pages

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,12 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     vue(),
     vueDevTools(),
   ],
+  base: mode === 'production' ? '/dish/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
